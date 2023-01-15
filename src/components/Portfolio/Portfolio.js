3a81aac5df545be9327ec8136918c5c7cc7cc459
Umar Ahmed <mian.ahmed@ucalgary.ca>
2023-01-14 22:56:03 -0700

    changed button styling onclick

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -9,15 +9,19 @@ import projectsData from './ProjectsData'
 export default function Portfolio() {
   const [data,setData] = React.useState(projectsData)
   const [filteredData,setFilteredData] = React.useState(data)
+
+  const [clickedButton,setClickedButton] = React.useState('All')
   
 
-  function filterProjects(id){
-    if (id === 'All'){
+  function filterProjects(e){
+    setClickedButton(e.target.id)
+    if (e.target.id === 'All'){
       setFilteredData(data)
     }
     else{
-      const filteredPlaces = data.filter((item) => item.language == id)
-      setFilteredData(filteredPlaces)
+      const filteredData = data.filter((item) => item.language === e.target.id)
+      setFilteredData(filteredData)
+      console.log(e.target)
     }
   }
 
@@ -26,9 +30,15 @@ export default function Portfolio() {
         <div className="projects-wrapper">
           <h2 className="projects-header">Portfolio</h2>
           <div className="projects-languages">
-            <button id={'All'} onClick= {(e) => filterProjects(e.target.id)}>All</button>
-            <button id={'React'} onClick= {(e) => filterProjects(e.target.id)}>React</button>
-            <button id={'Python'} onClick= {(e) => filterProjects(e.target.id)}>Python</button>
+            <button id={'All'} onClick= {(e) => filterProjects(e)} 
+              className={clickedButton === 'All' ? "projects-languages-button clicked" : "projects-languages-button"}
+            >All</button>
+            <button id={'React'} onClick= {(e) => filterProjects(e)}
+              className={clickedButton === 'React' ? "projects-languages-button clicked" : "projects-languages-button"}
+            >React</button>
+            <button id={'Python'} onClick= {(e) => filterProjects(e)}
+              className={clickedButton === 'Python' ? "projects-languages-button clicked" : "projects-languages-button"}
+            >Python</button>
           </div>
           <div className="projects-grid">
             {filteredData.map( (item,index) => (
