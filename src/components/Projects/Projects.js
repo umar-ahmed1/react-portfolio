6067e6f5384673070746b3d703563c3baf320ef4
Umar Ahmed <mian.ahmed@ucalgary.ca>
2023-01-14 22:07:23 -0700

    added filter button functionality

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,57 +1,37 @@
 import React from 'react'
 import './Projects.css'
 import Project from './Project' 
+import projectsData from './ProjectsData'
 
 
-const projectsData = [
-  {
-    title:"React Quiz App",
-    description:"my first project it is a quiz app in react",
-    language:"React",
-    link:"www.google.com",
-    github:"https://github.com/umar-ahmed1/react-quiz-app",
-    img:'./images/react-quiz-app.png'
-  },
-  {
-    title:"React Portfolio Website",
-    description:"I used React to make a website where i put all my stuff on it blablablab",
-    language:"React",
-    link:"www.google.com",
-    github:"https://github.com/umar-ahmed1/react-portfolio",
-    img:'https://i0.wp.com/www.alphr.com/wp-content/uploads/2022/03/How-to-Find-the-Model-Number-on-a-Laptop.jpg?resize=738%2C320&ssl=1',
-  },
-  {
-    title:"Python Snake Game",
-    description:"my first project",
-    language:"Python",
-    link:"www.google.com",
-    github:"www.github.com",
-    img:'https://i0.wp.com/www.alphr.com/wp-content/uploads/2022/03/How-to-Find-the-Model-Number-on-a-Laptop.jpg?resize=738%2C320&ssl=1',
-  },
-  {
-    title:"Python Machine Learning",
-    description:"my first project",
-    language:"Python",
-    link:"www.google.com",
-    github:"www.github.com",
-    img:'https://i0.wp.com/www.alphr.com/wp-content/uploads/2022/03/How-to-Find-the-Model-Number-on-a-Laptop.jpg?resize=738%2C320&ssl=1',
-  },
-
-]
 
 
 export default function Projects() {
+  const [data,setData] = React.useState(projectsData)
+  const [filteredData,setFilteredData] = React.useState(data)
+  
+
+  function filterProjects(id){
+    if (id === 'All'){
+      setFilteredData(data)
+    }
+    else{
+      const filteredPlaces = data.filter((item) => item.language == id)
+      setFilteredData(filteredPlaces)
+    }
+  }
+
   return (
     <section className="projects">
         <div className="projects-wrapper">
           <h2 className="projects-header">Projects</h2>
           <div className="projects-languages">
-            <button>All</button>
-            <button>React</button>
-            <button>Python</button>
+            <button id={'All'} onClick= {(e) => filterProjects(e.target.id)}>All</button>
+            <button id={'React'} onClick= {(e) => filterProjects(e.target.id)}>React</button>
+            <button id={'Python'} onClick= {(e) => filterProjects(e.target.id)}>Python</button>
           </div>
           <div className="projects-grid">
-            {projectsData.map( (item,index) => (
+            {filteredData.map( (item,index) => (
               <Project
                 key = {index}
                 img={item.img}
---
+++ src/components/Projects/ProjectsData.js
@@ -0,0 +1,37 @@
+const projectsData = [
+    {
+      title:"React Quiz App",
+      description:"my first project it is a quiz app in react",
+      language:"React",
+      link:"https://umar-ahmed1.github.io/react-quiz-app/",
+      github:"https://github.com/umar-ahmed1/react-quiz-app",
+      img:'./images/react-quiz-app.png'
+    },
+    {
+      title:"React Portfolio Website",
+      description:"I used React to make a website where i put all my stuff on it blablablab",
+      language:"React",
+      link:"www.google.com",
+      github:"https://github.com/umar-ahmed1/react-portfolio",
+      img:'https://i0.wp.com/www.alphr.com/wp-content/uploads/2022/03/How-to-Find-the-Model-Number-on-a-Laptop.jpg?resize=738%2C320&ssl=1',
+    },
+    {
+      title:"Python Snake Game",
+      description:"my first project",
+      language:"Python",
+      link:"www.google.com",
+      github:"www.github.com",
+      img:'https://i0.wp.com/www.alphr.com/wp-content/uploads/2022/03/How-to-Find-the-Model-Number-on-a-Laptop.jpg?resize=738%2C320&ssl=1',
+    },
+    {
+      title:"Python Machine Learning",
+      description:"my first project",
+      language:"Python",
+      link:"www.google.com",
+      github:"www.github.com",
+      img:'https://i0.wp.com/www.alphr.com/wp-content/uploads/2022/03/How-to-Find-the-Model-Number-on-a-Laptop.jpg?resize=738%2C320&ssl=1',
+    },
+  
+  ]
+
+  export default projectsData
