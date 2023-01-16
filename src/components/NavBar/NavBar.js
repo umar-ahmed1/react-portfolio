50b2bb7690eb83e3a45572fbed707dd5656b8de7
Umar Ahmed <mian.ahmed@ucalgary.ca>
2023-01-15 21:32:18 -0700

    added smooth scrolling to navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,35 +1,18 @@
 import React from 'react'
 import './NavBar.css'
+import { Link} from 'react-scroll'
 
 export default function NavBar() {
   const [navShow,setNavShow] = React.useState(true)
-  let prevScrollY = 0;
 
-  React.useEffect(()=>{
-    if (window.scrollY > 80){
-      setNavShow(false)
-    } else {
-      setNavShow(true)
-    }
-
-    prevScrollY = window.scrollY
- 
-  },[navShow])
 
   return (
     <nav className={navShow ? "navbar" : "navbar inactive"}>
-        <a href="#home">
-          <div className="nav-home">HOME</div>
-        </a>
-        <a href="#skills">
-          <div className="nav-about">SKILLS</div>
-        </a>
-        <a href="#projects">
-          <div className="nav-projects">PORTFOLIO</div>
-        </a>
-        <a href="#connect">
-          <div className="nav-connect">CONNECT</div>
-        </a>     
+      <Link className="nav-link" to="home" offset={-190} spy={true} smooth={true} duration={500}>HOME</Link>
+      <Link className="nav-link" to="skills" spy={true} smooth={true} duration={500}>SKILLS</Link>
+      <Link className="nav-link" to="projects" offset={-50} spy={true} smooth={true} duration={500}>PORTFOLIO</Link>
+      <Link className="nav-link" to="connect" spy={true} smooth={true} duration={500}>CONNECT</Link>
+
     </nav>
   )
 }
