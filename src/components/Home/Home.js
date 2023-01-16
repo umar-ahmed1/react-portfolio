61ee5e974e268c153214e7336da9c4fbea1723b2
Umar Ahmed <mian.ahmed@ucalgary.ca>
2023-01-15 20:05:07 -0700

    added typewriter effect to home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Home.css'
+import Typical from 'react-typical'
+import Typewriter from 'typewriter-effect'
 
 export default function About()  {
  return (
@@ -8,7 +10,16 @@ export default function About()  {
           <div className="profile-picture"></div>
           <div className="profile-details">
             <div className="name">Hi, I'm Umar Ahmed</div>
-            <div className="description">I am a Front-End Developer from Calgary, Alberta </div>
+            <div className="description">
+            <Typewriter
+              options={{
+                strings: ['I am a Front-End Developer from Calgary, Alberta.', 'I like to make web applications.','Welcome to my portfolio!'],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+              
+            </div>
             <button className="button cv">Download CV</button>
           </div>
           
