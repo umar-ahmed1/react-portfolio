4965eee354bb183ac7d6d199e2d505526adcf61f
Umar Ahmed <mian.ahmed@ucalgary.ca>
2023-01-19 01:26:10 -0700

    added temporary back to top button

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import './Footer.css'
+import { Link} from 'react-scroll'
 
 export default function Footer() {
   return (
-    <footer className="footer">Created by Umar Ahmed</footer>
+    <footer className="footer">
+        <div>Created by Umar Ahmed</div>
+        <Link  className="return-top" to="home" offset={-190} spy={true} smooth={true} duration={500}>
+            <div>Back to Top</div>
+            <img  src={require("./up_arrow.svg").default}></img>
+        </Link>
+            
+    
+    </footer>
+
   )
 }
