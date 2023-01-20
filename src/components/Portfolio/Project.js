6ea7cada65377ac1afe4b1d250a5b62608250338
Umar Ahmed <mian.ahmed@ucalgary.ca>
2023-01-20 01:08:46 -0700

    made the project cards flip on click

--- src/components/Portfolio/Project.js
+++ src/components/Portfolio/Project.js
@@ -1,19 +1,33 @@
 import React from 'react'
 
-export default function Project({title,description,link,github,img}) {
+export default function Project({title,description,link,github,img}) {  
+  const [flipped,setFlipped] = React.useState(false);
+  
+  function handleClick(e){
+    e.stopPropagation();
+    setFlipped(!flipped);
+
+  }
+
+
   return (
-    <div className="project-card">
-      <img className="project-img" src={img}></img>
-      <div className="project-card-title">{title}</div>
-      <div className="project-card-description">{description}</div>
-      <div className="project-card-links">
+    <div className={!flipped ? "project-card" : "project-card flipped"} onClick={(e) => handleClick(e)}>
+      <div className="project-card-front">
+        <img className="project-img" src={img}></img>
+        <div className="project-card-title">{title}</div>
+      </div>
+      <div className="project-card-back">
+        <div className="project-card-description">{description}</div>
+        <div className="project-card-links">
           <a target="_blank" href={link}>
-              <button className="project-card-view">View Project</button>  
+            <button className="project-card-view">View Project</button>  
           </a>
           <a className="project-card-github" target="_blank" href={github}>
-          <img className="github-svg" src={require("./images/github.svg").default}></img>
+            <img className="github-svg" src={require("./images/github.svg").default}></img>
           </a>     
+        </div>
       </div>
+
     </div>
   )
 }
