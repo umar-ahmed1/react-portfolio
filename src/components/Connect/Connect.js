9a01bba74b409783e1d2f5721655ee89db151a03
Umar Ahmed <mian.ahmed@ucalgary.ca>
2023-01-26 15:53:09 -0700

    some connect form styling

--- src/components/Connect/Connect.js
+++ src/components/Connect/Connect.js
@@ -33,40 +33,53 @@ export default function Connect() {
 
   return (
     <div className="connect" id="connect">
-      <div className="connect-header">Connect</div>
-      <div>LinkedIn</div>
-      <div>Github</div>
-      <div>Instagram</div>
-      <button>Email</button>
-
-      <form className="email-form" onSubmit={onSubmit}>
-        <div>Your Name</div>
-        <input
-          type='text'
-          name='from_name'
-          placeholder='Your Name'
-          value={emailInfo.from_name}
-          onChange={handleChange}
-        />
-        <div>Your Email</div>
-        <input
-          type='text'
-          name='reply_to'
-          placeholder='Your Email'
-          value={emailInfo.reply_to}
-          onChange={handleChange}
-        />
-        <div>Your Message</div>
-        <input
-          type='text'
-          name='message'
-          placeholder='Your message'
-          value={emailInfo.message}
-          onChange={handleChange}
-        />
+      
+      <div className="connect-content">
+        <div className="connect-left">
+          <div className="connect-header">Connect</div>
+          <div>I am currently seeking internships for the 2023-2024 year. Lets connect!</div>
+          <div className="phone-div">
+            <div>Phone</div>
+            <div>587-429-4194</div>
+          </div>
+          <div>LinkedIn</div>
+          <div>Test</div>
+        </div>
+        <div className="connect-right">
+          <div className="connect-header">Email Form</div>
+          <form className="email-form" onSubmit={onSubmit}>
+            <div>Your Name</div>
+            <input
+              type='text'
+              name='from_name'
+              placeholder='Your Name'
+              value={emailInfo.from_name}
+              onChange={handleChange}
+            />
+            <div>Your Email</div>
+            <input
+              type='text'
+              name='reply_to'
+              placeholder='Your Email'
+              value={emailInfo.reply_to}
+              onChange={handleChange}
+            />
+            <div>Your Message</div>
+            <input
+              type='text'
+              name='message'
+              placeholder='Your message'
+              value={emailInfo.message}
+              onChange={handleChange}
+            />   
+            <button type='submit'>Submit</button>
+          </form>
+        </div>
         
-      <button type='submit'>Submit</button>
-  </form>
+      </div>
+      
+
+      
 
     </div>
   )
