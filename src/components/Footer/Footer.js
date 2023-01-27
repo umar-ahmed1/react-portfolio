8dcd31e264f2c48f28fd883eb89f9177ad257930
Umar Ahmed <mian.ahmed@ucalgary.ca>
2023-01-26 21:58:12 -0700

    more media query

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import { Link} from 'react-scroll'
 export default function Footer() {
   return (
     <footer className="footer">
-        <div>Created by Umar Ahmed</div>
+        <div className="author">Created by Umar Ahmed</div>
         <Link  className="return-top" to="home" offset={-190} spy={true} smooth={true} duration={500}>
             <div>Back to Top</div>
             <img  src={require("./up_arrow.svg").default}></img>
